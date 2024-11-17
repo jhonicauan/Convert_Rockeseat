14dd8cfee4a3984356672c1aaadf40c05f0473cc
jhonielias <jhonielias8@gmail.com>
2024-11-17 17:49:26 -0300

    letras não permitidas

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const amount = document.querySelector("#amount");
 
 amount.addEventListener("input",()=>{
+    //usando regex para negar letras
+    const hasCharactersRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharactersRegex,"")
     console.log(amount.value);
 })
