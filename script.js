fbdbbed8a3314a65dfee079c178295d9bbcde53a
jhonielias <jhonielias8@gmail.com>
2024-11-17 17:55:26 -0300

    Pegando o tipo de moeda

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
 
 amount.addEventListener("input",()=>{
     //usando regex para negar letras
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex,"")
     console.log(amount.value);
+})
+
+currency.addEventListener("change",()=>
+{
+    const currencyValue = currency.value;
+    if(currencyValue == "EUR"){
+        console.log("Euro");
+    }else if(currencyValue == "USD"){
+        console.log("Dolar")
+    }else{
+        console.log("Libra Esterlina")
+    }
 })
