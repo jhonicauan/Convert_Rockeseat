b5232739acf49aad319f8389298a25c4417153f7
jhonielias <jhonielias8@gmail.com>
2024-11-17 22:30:02 -0300

    convertendo dinheiro

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
-
+const form = document.querySelector("form");
 amount.addEventListener("input",()=>{
     //usando regex para negar letras
     const hasCharactersRegex = /\D+/g;
@@ -8,14 +8,23 @@ amount.addEventListener("input",()=>{
     console.log(amount.value);
 })
 
-currency.addEventListener("change",()=>
+form.addEventListener("submit",(event)=>
 {
+    event.preventDefault();
     const currencyValue = currency.value;
+    let newPrice = null;
+    let money = amount.value;
     if(currencyValue == "EUR"){
-        console.log("Euro");
+        newPrice = convertCurrency(money,6.11,'€')
     }else if(currencyValue == "USD"){
-        console.log("Dolar")
+        newPrice = convertCurrency(money,5.8,'U$')
     }else{
-        console.log("Libra Esterlina")
+        newPrice = convertCurrency(money,7.33,'£')
     }
+    console.log(newPrice);
 })
+
+function convertCurrency(amount,price,symbol){
+    let newPrice = amount * price;
+    return `${symbol} ${price}`
+}
